6da7fbbf9384556c72e0829a5090bf240c779912
douagz <122457822+douagz@users.noreply.github.com>
2024-01-12 23:25:51 +0800

    Update load_baidu_search_data.py

--- common/load_baidu_search_data.py
+++ common/load_baidu_search_data.py
@@ -2,7 +2,7 @@ import yaml
 from config.config import Config
 
 
-def load_baidu_search_data() -> dict[str,str]:
+def load_baidu_search_data():
     with open(Config.baidu_search_data_file, 'r', encoding='utf-8') as file:
         data = yaml.load(file, Loader=yaml.FullLoader)
     return data
